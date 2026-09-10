2f9ff3e02dfad39c824ae05696e6c44200d6fe98
januszko4 <156959295+januszko4@users.noreply.github.com>
2026-09-10 09:32:38 +0200

    zadanie1

--- zadania/main.cpp
+++ zadania/main.cpp
@@ -1,9 +1,66 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <chrono>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+string aktualnaData() {
+    auto dzis = std::chrono::floor<std::chrono::days>(std::chrono::system_clock::now());
+    std::ostringstream oss;
+    oss << std::chrono::year_month_day{dzis};
+    return oss.str();
+}
+
+enum Level {
+    NONE,
+    LOW,
+    MEDIUM,
+    HIGH
+};
+
+class Notatka {
+public:
+    string tytul;
+    string tresc = "";
+    enum Level priorytet;
+    string dataDodania = aktualnaData();
+
+    Notatka(string ty, string tr, enum Level p) {
+        tytul = ty;
+        tresc = tr;
+        priorytet = p;
+    }
+
+    Notatka(string ty) {
+        tytul = ty;
+        priorytet = Level::NONE;
+    }
+
+    void info(){
+        cout << "---------------------" << '\n';
+        cout << "TYTUL - " << tytul << '\n';
+        cout << "TRESC - " << tresc << '\n';
+        cout << "DATA DODANIA - " << dataDodania << '\n';
+        cout << ((priorytet == 3) ? "Notatka WAZNA!" : "") << '\n';
+        cout << "---------------------" << '\n';
+    }
+    void setTitle(string newTitle) {
+        this->tytul = newTitle;
+    }
+
+};
+
+int main(){
+    Notatka n1("TYTULTEST3", "TRESCTEST", Level::HIGH);
+    Notatka n2("");
+
+    string newTitle = "";
+    cout << "Podaj tytul notatki: ";
+    cin >> newTitle;
+    n2.setTitle(newTitle);
+
+    n1.info();
+    n2.info();
     return 0;
 }
