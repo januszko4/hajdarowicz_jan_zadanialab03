cbf579acc24c3895b395dabf79ca224fae182739
januszko4 <156959295+januszko4@users.noreply.github.com>
2026-09-10 10:13:42 +0200

    zadanie2

--- zadania/main.cpp
+++ zadania/main.cpp
@@ -1,66 +1,116 @@
 #include <iostream>
 #include <string>
-#include <sstream>
-#include <chrono>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
-string aktualnaData() {
-    auto dzis = std::chrono::floor<std::chrono::days>(std::chrono::system_clock::now());
-    std::ostringstream oss;
-    oss << std::chrono::year_month_day{dzis};
-    return oss.str();
+class Kosc {
+private:
+    static int licznikInstancji;
+    static vector<string> nazwyPlikow;
+
+    int liczbaOczek;
+    int identyfikatorGrafiki;
+    bool dostepnosc;
+
+public:
+    Kosc(int wartosc) {
+        if (wartosc >= 1 && wartosc <= 6) {
+            liczbaOczek = wartosc;
+            identyfikatorGrafiki = wartosc;
+        } else {
+            liczbaOczek = 0;
+            identyfikatorGrafiki = 0;
+        }
+        dostepnosc = true;
+        licznikInstancji++;
+    }
+
+    Kosc() {
+        int wylosowana = rand() % 6 + 1; // [1; 6]
+        liczbaOczek = wylosowana;
+        identyfikatorGrafiki = wylosowana;
+        dostepnosc = true;
+        licznikInstancji++;
+    }
+
+
+    void rzuc() {
+        if (!dostepnosc) {
+            cout << "Kosc jest zablokowana\n";
+            return;
+        }
+        int wylosowana = rand() % 6 + 1;
+        liczbaOczek = wylosowana;
+        identyfikatorGrafiki = wylosowana;
+    }
+
+
+    void zablokuj() {
+        dostepnosc = false;
+    }
+
+
+    string wartoscTekstowa() const {
+        switch (liczbaOczek) {
+            case 1: return "jeden";
+            case 2: return "dwa";
+            case 3: return "trzy";
+            case 4: return "cztery";
+            case 5: return "piec";
+            case 6: return "szesc";
+            default: return "brak";
+        }
+    }
+
+    //getters
+    int getLiczbaOczek() const { return liczbaOczek; }
+    int getIdentyfikatorGrafiki() const { return identyfikatorGrafiki; }
+    bool getDostepnosc() const { return dostepnosc; }
+
+    string getNazwaPliku() const {
+        return nazwyPlikow[identyfikatorGrafiki];
+    }
+
+    static int getLicznikInstancji() {
+        return licznikInstancji;
+    }
+};
+
+int Kosc::licznikInstancji = 0;
+vector<string> Kosc::nazwyPlikow = {
+    "kosc0.png",
+    "kosc1.png",
+    "kosc2.png",
+    "kosc3.png",
+    "kosc4.png",
+    "kosc5.png",
+    "kosc6.png"
+};
+
+void wyswietlInfo(const Kosc& k) {
+    cout << "-----------------------------\n";
+    cout << "Liczba utworzonych instancji: " << Kosc::getLicznikInstancji() << '\n';
+    cout << "Liczba oczek: " << k.getLiczbaOczek() << '\n';
+    cout << "Wartosc tekstowa: " << k.wartoscTekstowa() << '\n';
+    cout << "Plik graficzny: " << k.getNazwaPliku() << '\n';
+    cout << "-----------------------------\n";
 }
 
-enum Level {
-    NONE,
-    LOW,
-    MEDIUM,
-    HIGH
-};
+int main() {
+    srand(time(0));
 
-class Notatka {
-public:
-    string tytul;
-    string tresc = "";
-    enum Level priorytet;
-    string dataDodania = aktualnaData();
+    Kosc kosc1;
+    wyswietlInfo(kosc1);
 
-    Notatka(string ty, string tr, enum Level p) {
-        tytul = ty;
-        tresc = tr;
-        priorytet = p;
-    }
+    int wartoscOdUzytkownika;
+    cout << "Podaj liczbe oczek (1-6): ";
+    cin >> wartoscOdUzytkownika;
 
-    Notatka(string ty) {
-        tytul = ty;
-        priorytet = Level::NONE;
-    }
+    Kosc kosc2(wartoscOdUzytkownika);
+    wyswietlInfo(kosc2);
 
-    void info(){
-        cout << "---------------------" << '\n';
-        cout << "TYTUL - " << tytul << '\n';
-        cout << "TRESC - " << tresc << '\n';
-        cout << "DATA DODANIA - " << dataDodania << '\n';
-        cout << ((priorytet == 3) ? "Notatka WAZNA!" : "") << '\n';
-        cout << "---------------------" << '\n';
-    }
-    void setTitle(string newTitle) {
-        this->tytul = newTitle;
-    }
-
-};
-
-int main(){
-    Notatka n1("TYTULTEST3", "TRESCTEST", Level::HIGH);
-    Notatka n2("");
-
-    string newTitle = "";
-    cout << "Podaj tytul notatki: ";
-    cin >> newTitle;
-    n2.setTitle(newTitle);
-
-    n1.info();
-    n2.info();
     return 0;
 }
