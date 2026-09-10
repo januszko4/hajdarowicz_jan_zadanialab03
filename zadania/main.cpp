4ddcb4a4ebaa77c995a5da416b8ae34096be95d8
januszko4 <156959295+januszko4@users.noreply.github.com>
2026-09-10 10:25:44 +0200

    zadanie 3

--- zadania/main.cpp
+++ zadania/main.cpp
@@ -1,116 +1,93 @@
 #include <iostream>
 #include <string>
-#include <vector>
-#include <cstdlib>
-#include <ctime>
-
 using namespace std;
 
-class Kosc {
-private:
-    static int licznikInstancji;
-    static vector<string> nazwyPlikow;
-
-    int liczbaOczek;
-    int identyfikatorGrafiki;
-    bool dostepnosc;
+class Pytanie {
+protected:
+    string trescPytania;
+    string nazwaPliku;
+    bool poprawnoscOdpowiedzi;
 
 public:
-    Kosc(int wartosc) {
-        if (wartosc >= 1 && wartosc <= 6) {
-            liczbaOczek = wartosc;
-            identyfikatorGrafiki = wartosc;
-        } else {
-            liczbaOczek = 0;
-            identyfikatorGrafiki = 0;
-        }
-        dostepnosc = true;
-        licznikInstancji++;
+    Pytanie(string tresc, string plik) {
+        trescPytania = tresc;
+        nazwaPliku = plik;
+        poprawnoscOdpowiedzi = false;
     }
 
-    Kosc() {
-        int wylosowana = rand() % 6 + 1; // [1; 6]
-        liczbaOczek = wylosowana;
-        identyfikatorGrafiki = wylosowana;
-        dostepnosc = true;
-        licznikInstancji++;
-    }
-
-
-    void rzuc() {
-        if (!dostepnosc) {
-            cout << "Kosc jest zablokowana\n";
-            return;
-        }
-        int wylosowana = rand() % 6 + 1;
-        liczbaOczek = wylosowana;
-        identyfikatorGrafiki = wylosowana;
-    }
-
-
-    void zablokuj() {
-        dostepnosc = false;
-    }
-
-
-    string wartoscTekstowa() const {
-        switch (liczbaOczek) {
-            case 1: return "jeden";
-            case 2: return "dwa";
-            case 3: return "trzy";
-            case 4: return "cztery";
-            case 5: return "piec";
-            case 6: return "szesc";
-            default: return "brak";
-        }
-    }
+    virtual bool sprawdzOdpowiedz(char odpowiedz) = 0;
 
     //getters
-    int getLiczbaOczek() const { return liczbaOczek; }
-    int getIdentyfikatorGrafiki() const { return identyfikatorGrafiki; }
-    bool getDostepnosc() const { return dostepnosc; }
+    string getTrescPytania() const { return trescPytania; }
+    string getNazwaPliku() const { return nazwaPliku; }
+    bool getPoprawnoscOdpowiedzi() const { return poprawnoscOdpowiedzi; }
 
-    string getNazwaPliku() const {
-        return nazwyPlikow[identyfikatorGrafiki];
-    }
-
-    static int getLicznikInstancji() {
-        return licznikInstancji;
-    }
 };
 
-int Kosc::licznikInstancji = 0;
-vector<string> Kosc::nazwyPlikow = {
-    "kosc0.png",
-    "kosc1.png",
-    "kosc2.png",
-    "kosc3.png",
-    "kosc4.png",
-    "kosc5.png",
-    "kosc6.png"
-};
+class PytanieZamkniete : public Pytanie {
+private:
+    string odpowiedzA;
+    string odpowiedzB;
+    string odpowiedzC;
+    char poprawnaOdpowiedz;
 
-void wyswietlInfo(const Kosc& k) {
-    cout << "-----------------------------\n";
-    cout << "Liczba utworzonych instancji: " << Kosc::getLicznikInstancji() << '\n';
-    cout << "Liczba oczek: " << k.getLiczbaOczek() << '\n';
-    cout << "Wartosc tekstowa: " << k.wartoscTekstowa() << '\n';
-    cout << "Plik graficzny: " << k.getNazwaPliku() << '\n';
-    cout << "-----------------------------\n";
-}
+public:
+    PytanieZamkniete(string tresc, string plik,
+                      string odpA, string odpB, string odpC,
+                      char poprawna)
+        : Pytanie(tresc, plik)
+    {
+        odpowiedzA = odpA;
+        odpowiedzB = odpB;
+        odpowiedzC = odpC;
+        poprawnaOdpowiedz = poprawna;
+    }
+
+
+    bool sprawdzOdpowiedz(char odpowiedz) override {
+        if (odpowiedz == poprawnaOdpowiedz) {
+            poprawnoscOdpowiedzi = true;
+        } else {
+            poprawnoscOdpowiedzi = false;
+        }
+        return poprawnoscOdpowiedzi;
+    }
+
+    void wyswietlPytanie() const {
+        cout << trescPytania << '\n';
+        cout << "A) " << odpowiedzA << '\n';
+        cout << "B) " << odpowiedzB << '\n';
+        cout << "C) " << odpowiedzC << '\n';
+    }
+};
 
 int main() {
-    srand(time(0));
 
-    Kosc kosc1;
-    wyswietlInfo(kosc1);
+    PytanieZamkniete pytanie1(
+        "Jaka jest stolica Polski?",
+        "pytanie1.png",
+        "Krakow",
+        "Warszawa",
+        "Gdansk",
+        'B'
+    );
 
-    int wartoscOdUzytkownika;
-    cout << "Podaj liczbe oczek (1-6): ";
-    cin >> wartoscOdUzytkownika;
+    pytanie1.wyswietlPytanie();
 
-    Kosc kosc2(wartoscOdUzytkownika);
-    wyswietlInfo(kosc2);
+    char odpowiedzUzytkownika;
+    cout << "Twoja odpowiedz (A/B/C): ";
+    cin >> odpowiedzUzytkownika;
+
+    bool wynik = pytanie1.sprawdzOdpowiedz(odpowiedzUzytkownika);
+
+    if (wynik) {
+        cout << "Poprawna odpowiedz!\n";
+    } else {
+        cout << "Niepoprawna odpowiedz.\n";
+    }
+
+    cout << "Pole poprawnoscOdpowiedzi: "
+         << (pytanie1.getPoprawnoscOdpowiedzi() ? "true" : "false") << '\n';
 
     return 0;
 }
